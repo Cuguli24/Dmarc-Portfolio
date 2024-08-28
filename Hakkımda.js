08bae4f5f8dea2097e5dbc0bc137ece1770aad7f
Yakup Aktepe <136591368+Cuguli24@users.noreply.github.com>
2024-08-28 17:07:51 +0300

    Add files via upload

---
+++ Hakkımda.js
@@ -0,0 +1,32 @@
+const cursorDot=document.querySelector(".cursor-dot");
+const cursorOutline=document.querySelector(".cursor-outline");
+
+window.addEventListener("mousemove",function(e){
+    const posX=e.clientX
+    const posy=e.clientY
+
+    cursorDot.style.left=posX.toString()+'px';
+    cursorDot.style.top=posy.toString()+'px'
+
+    //cursorOutline.style.left=posX.toString()+'px';
+    //cursorOutline.style.top=posy.toString()+'px'
+
+    cursorOutline.animate({
+        left:posX.toString()+'px',
+        top:posy.toString()+'px'
+    },{duration:500,fill:"forwards"});
+})
+
+function isTouchDevice() {
+    return ('ontouchstart' in window || navigator.maxTouchPoints > 0);
+}
+  
+if (isTouchDevice()) {
+    cursorDot.style.display="none";
+    cursorOutline.style.display="none";
+}
+else {
+    cursorDot.style.display="flex"
+    cursorOutline.style.display="flex"
+}
+  
